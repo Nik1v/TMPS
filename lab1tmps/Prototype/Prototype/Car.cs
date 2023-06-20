54e6d6b10ddc62cbba36337d41c730c5f415db83
Nik1v <137095339+Nik1v@users.noreply.github.com>
2023-06-20 15:13:25 +0300

    Add files via upload

---
+++ lab1tmps/Prototype/Prototype/Car.cs
@@ -0,0 +1,13 @@
+﻿public class Car : Vehicle
+{
+    public Car()
+    {
+        Wheels = 4;
+        Color = "Black";
+    }
+
+    public override Vehicle Clone()
+    {
+        return (Car)MemberwiseClone();
+    }
+}
