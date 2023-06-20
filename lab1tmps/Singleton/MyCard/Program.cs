5bffd6199cdac3e7ee2ca5a42cbb1527eb6ac3dc
Nik1v <137095339+Nik1v@users.noreply.github.com>
2023-06-20 15:15:24 +0300

    Add files via upload

---
+++ lab1tmps/Singleton/MyCard/Program.cs
@@ -0,0 +1,10 @@
+﻿// Obținem instanța Singleton
+DatabaseConnection connection = DatabaseConnection.Instance;
+
+// Conectăm la bază de date
+connection.Connect();
+
+// Alte operații cu baza de date
+
+// Deconectăm de la bază de date
+connection.Disconnect();
